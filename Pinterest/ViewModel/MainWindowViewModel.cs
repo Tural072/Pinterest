38d0ba951bbf20f850ed2a5ac1e1f40728250a0b
Tahi_ri96@itstep.edu.az <Tahi_ri96@itstep.edu.az>
2021-12-22 15:36:58 +0400

    .

---
+++ Pinterest/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinterest.ViewModel
+{
+    public class MainWindowViewModel:BaseViewModel
+    {
+        public MainWindowViewModel(MainWindow mainWindow)
+        {
+
+        }
+    }
+}
