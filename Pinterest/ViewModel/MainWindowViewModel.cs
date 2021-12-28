e71210d1f1ec9fb6210dccbdc6093817be16b971
Tahi_ri96@itstep.edu.az <Tahi_ri96@itstep.edu.az>
2021-12-28 09:33:37 +0400

    done

--- Pinterest/ViewModel/MainWindowViewModel.cs
+++ Pinterest/ViewModel/MainWindowViewModel.cs
@@ -1,15 +1,113 @@
-﻿using System;
+﻿using Pinterest.Commands;
+using Pinterest.Extencions;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace Pinterest.ViewModel
 {
     public class MainWindowViewModel:BaseViewModel
     {
+        public RelayCommand SelectBtnCommand { get; set; }
+        public RelayCommand SendBtnCommand { get; set; }
+
+
+
+
+        byte[] b;
+
+
+
+
         public MainWindowViewModel(MainWindow mainWindow)
         {
+
+
+
+
+
+            SelectBtnCommand = new RelayCommand((sender) =>
+            {
+
+
+
+                try
+                {
+                    var open = new Microsoft.Win32.OpenFileDialog();
+
+
+
+                    open.Multiselect = false;
+                    open.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    open.ShowDialog();
+                    b = ImageHelper.GetBytesOfImage(path: open.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+
+
+
+
+
+
+
+
+            });
+            SendBtnCommand = new RelayCommand((sender) =>
+            {
+                var client = new TcpClient();
+                var ip = IPAddress.Parse("10.1.18.29");
+                var port = 27001;
+                var ep = new IPEndPoint(ip, port);
+                try
+                {
+                    client.Connect(ep);
+                    if (client.Connected)
+                    {
+                        var writer = Task.Run(() =>
+                        {
+                            while (true)
+                            {
+
+
+
+                                var stream = client.GetStream();
+                                var bw = new BinaryWriter(stream);
+                                bw.Write(b);
+                            };
+                        });
+
+
+
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("CLinet doesnt connected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+
+            });
+
+
 
         }
     }
--- ServerPinterest/ViewModel/MainWindowViewModel.cs
+++ ServerPinterest/ViewModel/MainWindowViewModel.cs
@@ -1,16 +1,112 @@
-﻿using ServerPinterest;
+﻿using Pinterest.Extencions;
+using Pinterest.Models;
+using Pinterest.ViewModel;
+using ServerPinterest;
+using ServerPinterest.Commands;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
-namespace Pinterest.ViewModel
+namespace ServerPinterest.ViewModel
 {
     public class MainWindowViewModel:BaseViewModel
     {
+
+        public RelayCommand a { get; set; }
+        public List<Images> images { get; set; }
+        public string Path { get; set; }
+        static TcpListener listener = null;
+        static BinaryWriter bw = null;
+        static BinaryReader br = null;
+        int count = 0;
         public MainWindowViewModel(MainWindow mainWindow)
         {
+
+
+
+            images = new List<Images>();
+
+
+
+            a = new RelayCommand((sender) =>
+            {
+                Task.Run(() =>
+                {
+                    var ip = IPAddress.Parse("10.1.18.29");
+                    var ep = new IPEndPoint(ip, 27001);
+                    listener = new TcpListener(ep);
+                    listener.Start();
+
+
+
+                    while (true)
+                    {
+                        var client = listener.AcceptTcpClient();
+                        MessageBox.Show($"{client.Client.RemoteEndPoint} connected");
+                        byte[] bytes = new byte[50000000];
+                        Task.Run(() =>
+                        {
+                            var reader = Task.Run(() =>
+                            {
+                                ++count;
+                                var stream = client.GetStream();
+                                br = new BinaryReader(stream);
+                                bw = new BinaryWriter(stream);
+                                while (true)
+                                {
+                                    bytes = br.ReadBytes(count: 5000000);
+                                    var msg = br.ReadString();
+                                    Path = ImageHelper.GetImagePath(buffer: bytes, counter: count);
+                                    break;
+
+
+
+                                }
+                                App.Current.Dispatcher.Invoke(() =>
+                                {
+                                    mainWindow.myListView.Items.Add(new Images { ImagePath = Path });
+
+
+
+                                });
+                            });
+
+
+
+                        });
+
+
+
+
+
+                    }
+                });
+
+
+
+            });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
